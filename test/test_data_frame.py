06830c94f7671888267815a1f1a37c6448b59ecc
rogovski <michaeljrogowski@gmail.com>
2016-03-24 10:47:46 -0400

    path

--- test/test_data_frame.py
+++ test/test_data_frame.py
@@ -15,6 +15,6 @@ class TestDataFrameMethods(unittest.TestCase):
 
   def test_dict_list_keep(self):
       df = frame.Frame();
-      df.loadCsv('C:\\Users\\ESCO-1\\CodeRepos\\stream\\pycommondev\\pyCommon\\test\\test_data\\test_data_frame1.csv');
+      df.loadCsv('test_data/test_data_frame1.csv');
       keepsE = df.asDictListKeep(keep=['Corp'])
       self.assertEqual(len(keepsE.val), df._df.shape[0])
